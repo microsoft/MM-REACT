9608e10addc06144f6a701fd69dffbcf815164c3
Ehsan Azar <ehazar@microsoft.com>
2023-03-01 14:42:05 -0800

    wip

--- langchain/tools/imun/tool.py
+++ langchain/tools/imun/tool.py
@@ -10,7 +10,7 @@ class ImunRun(BaseTool):
     name = "Image Understanding"
     description = (
         "A wrapper around Image Understanding. "
-        "Useful for when you need to understand what is inside an image (objects, texts, people) for the first time. "
+        "Useful for when you need to understand what is inside an image (objects, texts, people)."
         "Input should be an image url, or any url to an image file (e.g. .jpg, .png)."
     )
     api_wrapper: ImunAPIWrapper
