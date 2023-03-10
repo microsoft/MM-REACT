c8d95682dc290c6617389845fe03c14e174b49ca
Ehsan Azar <ehazar@microsoft.com>
2023-03-09 19:20:44 -0800

    Revert "test prompt"
    This reverts commit d66c472bf4021169b56b31eafd24b1d3d33c1fe6.

--- langchain/agents/assistant/base.py
+++ langchain/agents/assistant/base.py
@@ -35,7 +35,7 @@ class AssistantAgent(Agent):
 
     @property
     def _stop(self) -> List[str]:
-        return [f"\n{self.observation_prefix}", "\nHuman:", "\nEND OF EXAMPLE", "\nNew input:"]
+        return [f"\n{self.observation_prefix}", "\nHuman:", "\nFor example:", "\nNew input:"]
 
     @classmethod
     def create_prompt(
@@ -55,7 +55,7 @@ class AssistantAgent(Agent):
         Returns:
             A PromptTemplate with the template assembled from the pieces here.
         """
-        template = "\n\n".join([prefix.format(ai_prefix=ai_prefix), suffix])
+        template = "\n\n".join([prefix.format(ai_prefix=ai_prefix, example_end_suffix=""), suffix])
         if input_variables is None:
             input_variables = ["input", "chat_history", "agent_scratchpad"]
         return PromptTemplate(template=template, input_variables=input_variables)
--- langchain/agents/assistant/prompt.py
+++ langchain/agents/assistant/prompt.py
@@ -5,8 +5,10 @@ Now let me introduce you to Assistant. He is great at understanding what is goin
 Any time there is an image in our conversation that you want to know about objects description, texts, OCR (optical character recognition), people, celebrities inside of the image you could ask Assistant by addressing him. 
 Make sure to provide Assistant with the best concise task that Assistant can handle. 
 
-EXAMPLE
+For example:
 {ai_prefix}: This is a business card image. Assistant, what OCR text do you see in this business card?  https://i.ibb.co/tsQ0Myn/00.jpg
+And Assistant will reply with:
+
 Assistant: This is an image (640 x 480)
 This image contains text
 
@@ -16,11 +18,10 @@ Cell: +1 (989) 123-4567
 Tel: +1 (989) 213-5674 Fax: +1 (989) 312-6745 4001 1st Ave NE Redmond, WA 98052
 Jake Smith Researcher Cloud & AI Department jake.smith@contoso.com https://www.contoso.com/
 
-END OF EXAMPLE
+In this example 640 480 are the width height dimension of the image
+{example_end_suffix}
 
-In above example 640 480 are the width height dimension of the image
-
-EXAMPLE
+For example:
 {ai_prefix}: This is an image. Assistant, what objects do you see in this image?  https://tinyurl.com/foo092001
 Assistant: This is an image (1920 x 1307) with description a group of men playing football.
 
@@ -35,13 +36,11 @@ athletic game
 football
 soccer
 
-END OF EXAMPLE
-
-In above example,
-1476, 993 is the left top coordinate of soccer ball, and 1595, 1125 is the right bottom coordinate of the soccer ball
-1300, 1200 is the left top coordinate of shoe, and 1510, 1290 is the right bottom coordinate of the shoe
-soccer ball area is width*height = (1595-1476)*(1125-993)
-1920 1307 is the width height dimension of the image
+In this example 1476, 993 is the left top coordinate of soccer ball, and 1595, 1125 is the right bottom coordinate of the soccer ball
+In this example 1300, 1200 is the left top coordinate of shoe, and 1510, 1290 is the right bottom coordinate of the shoe
+In this example, soccer ball area is width*height = (1595-1476)*(1125-993)
+In this example 1920 1307 is the width height dimension of the image
+{example_end_suffix}
 
 These are the tasks that Assistant can handle for an image: celebrities, landmarks, business card, receipt, objects, OCR, Bing
 If the task does not fit any of the above, make sure the question has the word objects in it.
@@ -51,7 +50,7 @@ For example, to ask about an image that could be a receipt, make sure the questi
 For example, to ask about an image that requires OCR (e.g. text, stamp, currency, money, dollar, paper, banknote in the image's object tags), make sure the question has the word OCR in it.
 For example, to ask about the identity of people (celebrities) in an image, make sure the question has the word celebrities in it, to see if any one of them are famous.
 
-EXAMPLE
+For example:
 Human: What is the result of the first equation in this image: https://i.ibb.co/YJbmKg4/somX1.png
 {ai_prefix}: This is an image with text. Assistant, what is the OCR texts in this image?  https://i.ibb.co/YJbmKg4/somX1.png
 Assistant: This is an image (616 x 411)
@@ -60,10 +59,9 @@ This image contains handwritten text
 List of texts (words) seen in this image:
 4x2+5=
 1/sqrt(9)=
+{example_end_suffix}
 
-END OF EXAMPLE
-
-EXAMPLE
+For example:
 Human: https://i.ibb.co/XJbmhg4/mY09.png
 {ai_prefix}: This is an image. Assistant, what objects do you see in this image? https://i.ibb.co/XJbmhg4/mY09.png
 Assistant: This is an image (656 x 500) with description a dog playing with a man.
@@ -84,9 +82,9 @@ Detected faces, and their location in this image:
 man 12, 100, 62, 202
 
 {ai_prefix}: There is a face detected in this image. Assistant, are there any celebrities in this image? https://i.ibb.co/XJbmhg4/mY09.png
-END OF EXAMPLE
+{example_end_suffix}
 
-EXAMPLE
+For example:
 Human: what do you know about this image? /tmp/path/to/x_d_0(2).jpg
 {ai_prefix}: This is an image. Assistant, what objects do you see in this image? /tmp/path/to/x_d_0(2).jpg
 Assistant: This is an image (1100 x 800) with description a bottle of medicine.
@@ -110,13 +108,13 @@ List of texts (words) seen in this image:
 SPRING VALLEY.
 Supports Health
 SUPPLEMENT
-
+{example_end_suffix}
 {ai_prefix}: This is medicine supplement pills by SPRING VALLEY
 Human: where can I buy this medicine? and how much is the price in Euros?
 {ai_prefix}: this question requires Bing search. Assistant, Bing search where can I buy SPRING VALLEY supplement pills? and how much is the price in Euros?
-END OF EXAMPLE
 
-EXAMPLE
+
+For example:
 Human: /a/c0%5/XX99096.jpg
 {ai_prefix}: This is an image. Assistant, what objects do you see in this image? /a/c0%5/XX99096.jpg
 Assistant: This is an image (480 x 1200) with description black and white text on a receipt
@@ -125,10 +123,8 @@ This image contains object tags
 
 List of object tags seen in this image:
 text
-
 {ai_prefix}: This image is likely a receipt or ticket. Let me ask for more information.
 {ai_prefix}: Assistant, what are the OCR texts in this receipt? /a/c0%5/XX99096.jpg
-END OF EXAMPLE
 """
 SUFFIX = """
 
